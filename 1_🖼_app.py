7e3c6ac5e29395513caa8df228f3e031528da43f
Dominik Benk <56314830+dominikbenk@users.noreply.github.com>
2022-11-19 11:22:49 +0100

    Update and rename app.py to 1_🖼_app.py

--- 1_🖼_app.py
+++ 1_🖼_app.py
@@ -1,30 +1,30 @@
 import os
-import pickle
 import streamlit as st
 import requests
 
-os.environ["REPLICATE_API_TOKEN"] = 'asdas'
+st.set_page_config(
+    page_title="Text-to-image app",
+    layout="centered",
+    initial_sidebar_state="expanded"
+)
 
-# st.set_page_config(layout="wide")
-
-st.title('Stable diffusion test')
+st.title('Text-to-image generator')
 
 with st.form(key='form'):
-
+    token = st.text_input()
     prompt = st.text_area(label='Enter prompt')
-    
     st.form_submit_button()
 
-
-if prompt:
+if prompt and token:
     r = requests.post(
         "https://api.deepai.org/api/text2img",
         data={
             'text': prompt,
         },
-        headers={'api-key': 'quickstart-QUdJIGlzIGNvbWluZy4uLi4K'}
+        headers={'api-key': token}
     )
     image_url = r.json()['output_url']
     st.image(image_url)
+    
 else:
-    st.warning('Enter prompt.')
+    st.warning('Enter prompt and token.')
